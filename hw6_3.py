e12847aa0e932409b80d6efae8438eac3493bd1b
pigxinyi <49387709+pigxinyi@users.noreply.github.com>
2019-04-10 09:04:43 -0500

    Update hw6_3.py

--- hw6_3.py
+++ hw6_3.py
@@ -5,7 +5,7 @@ from torch.autograd import Variable
 import torchvision
 import torchvision.transforms as transforms
 import numpy as np
-import hw6_2
+
 
 import matplotlib
 matplotlib.use('Agg')
@@ -13,6 +13,97 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import os
 batch_size=128
+
+class discriminator(nn.Module):
+   
+        
+    def __init__(self):
+        super(discriminator, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=196, kernel_size=3, stride=1, padding=1) 
+        self.conv2 = nn.Conv2d(in_channels=196, out_channels=196, kernel_size=3, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=196, out_channels=196, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=196, out_channels=196, kernel_size=3, stride=2, padding=1)
+        self.conv5 = nn.Conv2d(in_channels=196, out_channels=196, kernel_size=3, stride=1, padding=1)
+        self.conv6 = nn.Conv2d(in_channels=196, out_channels=196, kernel_size=3, stride=1, padding=1)
+        self.conv7 = nn.Conv2d(in_channels=196, out_channels=196, kernel_size=3, stride=1, padding=1)
+        self.conv8 = nn.Conv2d(in_channels=196, out_channels=196, kernel_size=3, stride=2, padding=1)
+
+        
+        self.layernorm1 = nn.LayerNorm((196, 32, 32))
+        self.layernorm2 = nn.LayerNorm((196, 16, 16))
+        self.layernorm3 = nn.LayerNorm((196, 16, 16))
+        self.layernorm4 = nn.LayerNorm((196, 8, 8))
+        self.layernorm5 = nn.LayerNorm((196, 8, 8))
+        self.layernorm6 = nn.LayerNorm((196, 8, 8))
+        self.layernorm7 = nn.LayerNorm((196, 8, 8))
+        self.layernorm8 = nn.LayerNorm((196, 4, 4))
+
+        self.fc1 = nn.Linear(196, 1)
+        self.fc10 = nn.Linear(196, 10)
+    def set(self,layer=0):
+        self.extract_features =layer
+    def forward(self, x):
+        if self.extract_features == 8:
+            x = self.conv1( x )
+            x = F.leaky_relu(self.layernorm1( x ))
+            x = self.conv2( x )
+            x = F.leaky_relu(self.layernorm2( x ))
+            x = self.conv3( x )
+            x = F.leaky_relu(self.layernorm3( x ))
+            x = self.conv4( x )
+            x = F.leaky_relu(self.layernorm4( x ))
+            x = self.conv5( x )
+            x = F.leaky_relu(self.layernorm5( x ))
+            x = self.conv6( x )
+            x = F.leaky_relu(self.layernorm6( x ))
+            x = self.conv7( x )
+            x = F.leaky_relu(self.layernorm7( x ))
+            x = self.conv8( x )
+            x = F.leaky_relu(self.layernorm8( x ))
+    
+            x = F.max_pool2d(x, kenel_size=4, stride=4)
+            x = x.view(-1, 196)
+            return x
+
+       else if self.extract_features == 4:
+            x = self.conv1( x )
+            x = F.leaky_relu(self.layernorm1( x ))
+            x = self.conv2( x )
+            x = F.leaky_relu(self.layernorm2( x ))
+            x = self.conv3( x )
+            x = F.leaky_relu(self.layernorm3( x ))
+            x = self.conv4( x )
+            x = F.leaky_relu(self.layernorm4( x ))
+
+            x = F.max_pool2d(x, kernel_size=8, stride=8)
+            x = x.view(-1, 196)
+            return x
+        else:
+            x = self.conv1( x )
+            x = F.leaky_relu(self.layernorm1( x ))
+            x = self.conv2( x )
+            x = F.leaky_relu(self.layernorm2( x ))
+            x = self.conv3( x )
+            x = F.leaky_relu(self.layernorm3( x ))
+            x = self.conv4( x )
+            x = F.leaky_relu(self.layernorm4( x ))
+            x = self.conv5( x )
+            x = F.leaky_relu(self.layernorm5( x ))
+            x = self.conv6( x )
+            x = F.leaky_relu(self.layernorm6( x ))
+            x = self.conv7( x )
+            x = F.leaky_relu(self.layernorm7( x ))
+            x = self.conv8( x )
+            x = F.leaky_relu(self.layernorm8( x ))
+        
+            x = F.max_pool2d(x, kernel_size=4, stride=4)
+            
+            x = x.view(x.size(0), -1)
+            fc1_x = self.fc1(x)
+            fc10_x = self.fc10(x)
+            return fc1_x, fc10_x
+
+        
 def plot(samples):
     fig = plt.figure(figsize=(10, 10))
     gs = gridspec.GridSpec(10, 10)
